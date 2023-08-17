4d8f0ad7955fe5b576eb8afc058cb191f33d6fb1
Leonardo Barros Leite <leonardo.leite@al.senac.br>
2023-08-17 14:46:14 -0300

    Adicionando o cadastro de comandas

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import view_home, pagina_pratos
+from .views import *
 
 urlpatterns =[
     path('home/', view_home),
-    path('lista_de_pratos/', pagina_pratos)
+    path('lista_de_pratos/', pagina_pratos),
+    path('pagina_cadastro/', pagina_cadastro),
+    path('cadastrar_prato/', cadastrar_prato, name='cadastrar'),
+    path('pagina_cadastro_comanda/', pagina_cadastro_comanda),
+    path('cadastro_comanda/', cadastro_comanda, name='cadastro_comanda')
 ]
